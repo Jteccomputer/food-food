9b182251938ad07694f394e34304ea2a57aa4977
unknown <chukwukachinedujohn@gmail.com>
2024-02-19 22:44:26 +0100

    Added the Thumbnails and more functionalities

---
+++ src/components/Thumbnails/Thumbnails.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import classes from './thumbnails.module.css';
+import { Link } from 'react-router-dom';
+
+export default function Thumbnails({ foods }) {
+  return (
+            <ul className={classes.list}>
+              { foods.map(food => (
+                <li key={food.id}>
+                  <Link to ={`/food/${food.id}`}>
+                      <img
+                          className={classes.image}
+                          src={`/foods/${food.imageUrl}`}
+                          alt={food.name}
+                      />
+                  </Link>
+                      <div className={classes.content}> 
+                      <div className={ classes.name}>{ food.name}</div>
+                      <span className={`${classes.favorite} ${food.favorite ? '': classes.not}`}
+
+                      >
+                        ❤ 
+                      </span>
+
+                      </div>
+                </li>
+                ))}
+
+            </ul>
+    );
+};
---
+++ src/data.js
@@ -0,0 +1,81 @@
+export const sample_foods = [
+
+  {
+    id:'1',
+    name: "Pizza Peperoni",
+    price:10,
+    cookTime: "10-20",
+    favorite:false,
+    origins: ["Italy"],
+    stars: 4.5,
+    imageUrl: "food-1.jpeg",
+    tags:["fastfood", "Pizza", "Lunch"],
+
+  },
+  
+    {
+    id:'2',
+    name: "MeatBall",
+    price:20,
+    cookTime: "20-30",
+    favorite:true,
+    origins: ["Persia", "Middle east", "China"],
+    stars: 5,
+    imageUrl: "food-2.jpeg",
+    tags:["Slow Food", "Lunch"],
+
+  },
+
+  {
+    id:'3',
+    name: "hamburger",
+    price:5,
+    cookTime: "20-30",
+    favorite:false,
+    origins: ["Germany", "US"],
+    stars: 3.5,
+    imageUrl: "food-3.jpeg",
+    tags:["FastFood", "Hamburger"],
+
+  },
+  
+  {
+    id:'4',
+    name: "Fried Potatoes",
+    price:2,
+    cookTime: "15-20",
+    favorite:true,
+    origins: ["Belgium", "France"],
+    stars: 3,
+    imageUrl: "food-4.jpeg",
+    tags:["FastFood", "Fry"],
+
+  },
+
+  {
+    id:'5',
+    name: "Chicken Soup",
+    price:11,
+    cookTime: "40-50",
+    favorite:false,
+    origins: ["India", "Asia"],
+    stars: 3.5,
+    imageUrl: "food-5.jpeg",
+    tags:["SlowFood", "Soup"],
+
+  },
+
+  {
+    id:'6',
+    name: "Vegetables Pizza",
+    price:9,
+    cookTime: "40-50",
+    favorite:false,
+    origins: ["Italy"],
+    stars: 4.0,
+    imageUrl: "food-6.jpeg",
+    tags:["FastFood", "Pizza","Lunch"],
+
+  }
+
+];
