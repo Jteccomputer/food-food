688cf83198967ce737ea9ea7cd3fa1bc0d7e9735
unknown <chukwukachinedujohn@gmail.com>
2024-04-02 16:51:04 +0100

    Completed the Thumbnails and worked on other functionalities

--- src/components/Thumbnails/Thumbnails.js
+++ src/components/Thumbnails/Thumbnails.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import classes from './thumbnails.module.css';
+import StarRating from "../StarRating/StarRating";
+import Price from '../Price/Price';
 import { Link } from 'react-router-dom';
 
 export default function Thumbnails({ foods }) {
@@ -11,18 +13,39 @@ export default function Thumbnails({ foods }) {
                       <img
                           className={classes.image}
                           src={`/foods/${food.imageUrl}`}
-                          alt={food.name}
+                          alt={food.name} 
                       />
-                  </Link>
+                  
                       <div className={classes.content}> 
                       <div className={ classes.name}>{ food.name}</div>
                       <span className={`${classes.favorite} ${food.favorite ? '': classes.not}`}
 
                       >
-                        ❤ 
+                        ❤  
                       </span>
 
+                      <div className={classes.stars}>
+                        <StarRating stars={food.stars}/>
+
                       </div>
+                      <div className={classes.product_item_footer}>
+                        <div className={classes.origins}>
+                            {food.origins.map(origin => (
+                              <span key={origin}>{origin}</span>
+                            ))}
+
+                        </div>
+
+                        <div className={classes.cook_time}>
+                              <span>⏲</span>
+                              {food.cookTime}
+                        </div>
+                      </div>
+                      <div className={classes.price}>
+                              <Price price={food.price} />
+                      </div>
+                      </div>
+                  </Link>
                 </li>
                 ))}
 
